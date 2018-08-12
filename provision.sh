593e07706fd07a48096e6914d0150ad5d178ad8c
Michael <michael@managedkaos.com>
2018-08-12 14:10:03 -0700

    Fix virtualenv

--- provision.sh
+++ provision.sh
@@ -12,19 +12,20 @@ cp /vagrant/django.conf /etc/nginx/conf.d
 
 # Create an SSL key and certificate
 openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 \
-	-keyout /etc/ssl/private/nginx.key \
-	-out /etc/ssl/certs/nginx.crt 2>/dev/null
+    -keyout /etc/ssl/private/nginx.key \
+    -out /etc/ssl/certs/nginx.crt 2>/dev/null
 
 # Load the configuration
 systemctl reload nginx
 
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+# Setup and run Django
+curl -s https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python /tmp/get-pip.py
 pip install virtualenv virtualenvwrapper
 echo "export WORKON_HOME=~/Env" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-source ~/.bashrc
+export WORKON_HOME=~/Env
+source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv django
 pip install django
 cd /mnt && django-admin.py startproject website
@@ -32,4 +33,4 @@ cd /mnt/website && sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['*'\]/g" ./w
 cd /mnt/website && echo 'STATIC_ROOT = os.path.join(BASE_DIR, "static/")' >> ./website/settings.py
 cd /mnt/website && ./manage.py migrate
 cd /mnt/website && ./manage.py collectstatic
-
+cd /mnt/website && ./manage.py runserver 0.0.0.0:8080 &
